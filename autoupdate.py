dd9cf88b596e173fbfacfcded91599a5d52d1bcc
Caleb Farrell <caleb.farrell987@gmail.com>
2020-10-15 19:03:01 -0300

    Added: logging logic Added: new switches to the config.json allowing you to turn logging on and off as well as debugging mode, this will disable SteamCMD from running to debug issues with the program itself quicker. NOTE: debuggingMode will be removed from the master branch in the future Added: logic for identifying publisherIDs with a 0 value and outputting path to log file Added: switch to disable server updating

--- autoupdate.py
+++ autoupdate.py
@@ -1,21 +1,20 @@
-#Valkyrie-AutoUpdate
-#Written By: Caleb 'Crow' Farrell
-#Date: 2020 - 10 - 15
+# Valkyrie-AutoUpdate
+# Written By: Caleb 'Crow' Farrell
+# Date: 2020 - 10 - 15
 
-#TODO: Create output log
-#DONE: #TODO: prevent any folder W/O meta.cpp file from crashing the program
-#TODO: allow for both anon and signed in use of SteamCMD
-#TODO: Create a system for users to download new mods by checking a file and referencing it to the array of existing ones
+# TODO: allow for both anon and signed in use of SteamCMD
+# TODO: Create a system for users to download new mods by checking a file and referencing it to the array of existing
 
 import glob
 import re
 import os
 import time
 import json
+import logging
 
-#config stuffs
+
+# config stuffs
 configFile = 'config.json'
-
 with open(configFile, 'r') as f:
     datastore = json.load(f)
 
@@ -23,39 +22,53 @@ steamCMDPath = datastore["steamCmdPath"]
 serverPath = datastore["arma3ServerPath"]
 modsRepoPath = datastore["modsRepoPath"]
 
-#Arma 3 SteamID
+serverUpdateEnabled = int(datastore["serverUpdateEnabled"])
+debuggingMode = int(datastore["debuggingMode"])
+# Logging Stuffs
+loggingEnabled = int(datastore["loggingEnabled"])
+
+if loggingEnabled == 1:
+
+    if not os.path.exists('./logs'):
+        os.mkdir('./logs')
+
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='./logs/' + time.strftime("%Y-%m-%d %H-%M-%S_") + 'OUTPUT.log', level=logging.DEBUG)
+else:
+    logging.warning("LOGGING IS DISABLED")
+
+logging.info('Initializing...')
+# Arma 3 SteamID
 appId = '107410'
-#Arma3 Server SteamID
+# Arma3 Server SteamID
 serverAppId = '233780'
-#Arma 3 Workshop Mod ID
+# Arma 3 Workshop Mod ID
 publishedId = []
-#ALL Mods should have a meta.cpp file, if not at the moment this will crash the script.
-#I'm to lazy to actually make this not happen and skip the files that don't. suck it up.
+
 metaFile = "meta.cpp"
-#This should reference where you store all your mods,
-# AGAIN STORE THEM ALL IN A SEPERATE FOLDER
-# AS SAID ABOVE FAILURE TO DO SO WILL CRASH THE SCRIPT
+# This should reference where you store all your mods,
+
+logging.info('Complete!')
 
 print(" Mods Repo Path is: " + modsRepoPath)
 
-#Getting all mod folders
+# Getting all mod folders
 modFolders = glob.glob(modsRepoPath + "/*/")
 
 print(modFolders)
-
-#This is reading the meta.cpp for the publisherID and appending it to our array
+# This is reading the meta.cpp for the publisherID and appending it to our array
 
 for item in modFolders:
     print(item)
-    if os.path.exists(item +'./' + metaFile):
+    if os.path.exists(item + './' + metaFile):
         f = open(item + metaFile)
         lines = (f.readlines())
         publishedId.append(lines[1])
     else:
-        print("meta.cpp does not exist in:" + item + "If mod folder please re-download manually ")
-#here we define a new array for the cleansed publisherIDs
+        logging.warning("meta.cpp does not exist in:" + item + "If mod folder please re-download manually")
+# here we define a new array for the cleansed publisherIDs
 publishedIDs = []
-#Thanks to ZE JEW #9273 on Discord for the assistance on this one!
+
+# Thanks to ZE JEW #9273 on Discord for the assistance on this one!
 for i in publishedId:
     publishedIDs.append(
         int(
@@ -65,18 +78,35 @@ for i in publishedId:
         )
     )
 
-print(publishedIDs)
+# WIP may not work as intended yet
+# if you don't like it, delete it
 
-#Running the steamCMD to update Arma server
+indices = []
+for i in range(len(publishedIDs)):
+    if publishedIDs[i] == 0:
+        indices.append(i)
+
+nullMeta = [modFolders[index] for index in indices]
+logging.warning("PublishedID is Null in:" + str(nullMeta))
+
+print(publishedIDs)
+# Running the steamCMD to update Arma server
 print(steamCMDPath)
 
 print(serverPath)
 
 
-os.system(steamCMDPath+'\\'+'SteamCMD +login anonymous + force_install_dir' + serverPath + '+app_update' + serverAppId + 'validate +quit')
+if serverUpdateEnabled == 1:
+    os.system(steamCMDPath + '\\' + 'SteamCMD +login anonymous + force_install_dir' + serverPath + '+app_update' + serverAppId + 'validate +quit')
+else:
+    logging.warning("Server Update is Disabled!")
 
-for item in publishedIDs:
-    print(item)
-    os.system(steamCMDPath+'\\'+'SteamCMD +login anonymous + force_install_dir' + serverPath + '+workshop_download_item' + appId + str(item) + 'validate +quit')
-    time.sleep(15)
-
+if not debuggingMode == 1:
+    for item in publishedIDs:
+        print(item)
+        os.system(
+            steamCMDPath + '\\' + 'SteamCMD +login anonymous + force_install_dir' + serverPath + '+workshop_download_item' + appId + str(
+                item) + 'validate +quit')
+        time.sleep(15)
+else:
+    logging.warning("Debugging Mode Enabled!")
